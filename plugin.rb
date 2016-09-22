12f6f76ee530e3fe33178fbe1d9e2df1adbc22a8
Michał Warda <michal.warda@elpassion.pl>
2016-09-22 15:18:28 +0200

    Start adding net/http

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,36 @@
-require 'json'
+require 'net/http'
 
 Neovim.plugin do |plug|
+  comments = { 5 => { text: "This is a comment." } }
+
+  plug.command(:PlaceSigns, nargs: 0) do |nvim|
+    res = Net::HTTP.get_response(URI('https://api.github.com/repos/michalwarda/ghpr/pulls'))
+
+    comments.each do |line, _comment|
+      nvim.command("sign place #{line + 666} line=#{line} name=neomake_err file=#{nvim.current.buffer.name}")
+    end
+  end
+
+  plug.command(:UnplaceSigns, nargs: 0) do |nvim|
+    comments.each do |line, _comment|
+      nvim.command("sign unplace #{line + 666}")
+    end
+  end
+
+  plug.autocmd(:BufEnter) do |nvim|
+    nvim.command('UnplaceSigns')
+    nvim.command('PlaceSigns')
+  end
+
+  plug.autocmd(:BufWrite) do |nvim|
+    nvim.command('UnplaceSigns')
+    nvim.command('PlaceSigns')
+  end
+
   state = {
     previous_buffer: nil,
     previous_line: nil,
   }
-
-  comments = {
-    5 => { text: 'To nie ma sensu' }
-  }
-
-  # Define an autocmd for the BufEnter event on Ruby files.
-  plug.autocmd(:BufEnter) do |nvim|
-    comments.each do |line, _comment|
-      nvim.command("sign place 2 line=#{line} name=neomake_warn file=#{nvim.current.buffer.name}")
-    end
-  end
 
   plug.autocmd(:CursorMoved) do |nvim|
     new_buffer = nvim.current.buffer
@@ -23,7 +38,7 @@ Neovim.plugin do |plug|
     comment = comments[new_line]
 
     if comment && (new_buffer != state[:previous_buffer] || new_line != state[:previous_line])
-      nvim.command("echo 'PR: #{comment[:text]}'")
+      nvim.command("echo 'GHPR: #{comment[:text]}'")
     end
 
     state[:previous_buffer] = new_buffer
